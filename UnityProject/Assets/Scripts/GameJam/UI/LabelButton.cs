5b42bcf1f97f0b7418dd2b56f265f8890bd71f6f
redseiko <redseiko@gmail.com>
2024-09-11 20:33:03 -0700

    Create a basic tween for LabelButton.

--- UnityProject/Assets/Scripts/GameJam/UI/LabelButton.cs
+++ UnityProject/Assets/Scripts/GameJam/UI/LabelButton.cs
@@ -1,12 +1,17 @@
 using Coffee.UIEffects;
 
+using DG.Tweening;
+
 using TMPro;
 
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
+using YoloBox;
+
 namespace GameJam {
-  public sealed class LabelButton : MonoBehaviour {
+  public sealed class LabelButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
     [field: Header("Container")]
     [field: SerializeField]
     public RectTransform RectTransform { get; private set; }
@@ -21,5 +26,29 @@ namespace GameJam {
     [field: Header("Label")]
     [field: SerializeField]
     public TextMeshProUGUI Label { get; private set; }
+
+    Sequence _onHoverTween;
+
+    private void Start() {
+      CreateTweens();
+    }
+
+    void CreateTweens() {
+      _onHoverTween =
+          DOTween.Sequence()
+              .Insert(0f, Label.rectTransform.DOBlendableScaleBy(Vector3.one * 0.05f, 0.5f))
+              .SetLink(gameObject)
+              .SetAutoKill(false)
+              .SetUpdate(true)
+              .Pause();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) {
+      _onHoverTween.PlayAgain();
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+      _onHoverTween.SmoothRewind();
+    }
   }
 }
